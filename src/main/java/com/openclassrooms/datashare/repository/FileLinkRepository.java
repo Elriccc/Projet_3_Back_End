be84f33e3036fbddc0b57197b3c80206319aefbd
elrixtrixx <elric.finot@outlook.fr>
2026-03-03 20:53:56 +0100

    feat: ajout d'une méthode getFileLinksByUser

--- src/main/java/com/openclassrooms/datashare/repository/FileLinkRepository.java
+++ src/main/java/com/openclassrooms/datashare/repository/FileLinkRepository.java
@@ -1,9 +1,13 @@
 package com.openclassrooms.datashare.repository;
 
 import com.openclassrooms.datashare.entities.FileLink;
+import com.openclassrooms.datashare.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FileLinkRepository extends JpaRepository<FileLink, Long> {
+    List<FileLink> getFileLinksByUser(User user);
 }
