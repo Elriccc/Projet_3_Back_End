489c045ca97c83331d331e7276eada85cdbc0a40
elrixtrixx <elric.finot@outlook.fr>
2026-03-03 04:18:02 +0100

    test: ajout des tests pour l'US04

--- src/test/java/com/openclassrooms/datashare/controller/UserControllerTest.java
+++ src/test/java/com/openclassrooms/datashare/controller/UserControllerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
@@ -29,8 +28,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @AutoConfigureMockMvc
 @Testcontainers
 public class UserControllerTest {
+
     @Container
-    static PostgreSQLContainer psqlContainer = new PostgreSQLContainer("postgres:18.3");
+    static PostgreSQLContainer<?> psqlContainer = new PostgreSQLContainer<>("postgres:18.3");
 
     private static final String URL_REGISTER = "/api/register";
     private static final String URL_LOGIN = "/api/login";
@@ -41,7 +41,7 @@ public class UserControllerTest {
     private UserService service;
     @Autowired
     private UserRepository repository;
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
     private MockMvc mockMvc;
 
@@ -113,6 +113,106 @@ public class UserControllerTest {
                             .accept(MediaType.APPLICATION_JSON))
                     .andDo(print())
                     .andExpect(MockMvcResultMatchers.status().isCreated());
+        }
+    }
+
+    @Nested
+    @DisplayName("Se connecter")
+    class LoginTest {
+        @DisplayName("Sans mettre de login renvoie une erreur")
+        @Test
+        public void loginUserNoLogin() throws Exception {
+            // GIVEN
+            UserDTO userDTO = new UserDTO();
+            userDTO.setPassword(PASSWORD);
+
+            // WHEN
+            mockMvc.perform(MockMvcRequestBuilders.post(URL_LOGIN)
+                            .param("password", userDTO.getPassword())
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        }
+
+        @DisplayName("Sans mettre de mot de passe renvoie une erreur")
+        @Test
+        public void loginUserNoPassword() throws Exception {
+            // GIVEN
+            UserDTO userDTO = new UserDTO();
+            userDTO.setLogin(LOGIN);
+
+            // WHEN
+            mockMvc.perform(MockMvcRequestBuilders.post(URL_LOGIN)
+                            .param("login", userDTO.getLogin())
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        }
+
+        @DisplayName("À un utilisateur qui n'existe pas renvoie une erreur")
+        @Test
+        public void loginUserUnknownUser() throws Exception {
+            // GIVEN
+            UserDTO userDTO = new UserDTO();
+            userDTO.setLogin(LOGIN);
+            userDTO.setPassword(PASSWORD);
+
+            // WHEN
+            mockMvc.perform(MockMvcRequestBuilders.post(URL_LOGIN)
+                            .param("login", userDTO.getLogin())
+                            .param("password", userDTO.getPassword())
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        }
+
+        @DisplayName("Avec un mauvais mot de passe renvoie une erreur")
+        @Test
+        public void loginUserBadPassword() throws Exception {
+            // GIVEN
+            final String BAD_PASSWORD = "Badpassword";
+            UserDTO registerDTO = new UserDTO();
+            registerDTO.setLogin(LOGIN);
+            registerDTO.setPassword(PASSWORD);
+            mockMvc.perform(MockMvcRequestBuilders.post(URL_REGISTER)
+                    .content(objectMapper.writeValueAsString(registerDTO))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON));
+            UserDTO userDTO = new UserDTO();
+            userDTO.setLogin(LOGIN);
+            userDTO.setPassword(BAD_PASSWORD);
+
+            // WHEN
+            mockMvc.perform(MockMvcRequestBuilders.post(URL_LOGIN)
+                            .param("login", userDTO.getLogin())
+                            .param("password", userDTO.getPassword())
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        }
+
+        @DisplayName("Avec des informations correctes fonctionne")
+        @Test
+        public void loginUserSuccessful() throws Exception {
+            // GIVEN
+            UserDTO registeredUserDTO = new UserDTO();
+            registeredUserDTO.setLogin(LOGIN);
+            registeredUserDTO.setPassword(PASSWORD);
+            mockMvc.perform(MockMvcRequestBuilders.post(URL_REGISTER)
+                    .content(objectMapper.writeValueAsString(registeredUserDTO))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON));
+            UserDTO userDTO = new UserDTO();
+            userDTO.setLogin(LOGIN);
+            userDTO.setPassword(PASSWORD);
+
+            // WHEN
+            mockMvc.perform(MockMvcRequestBuilders.post(URL_LOGIN)
+                            .param("login", userDTO.getLogin())
+                            .param("password", userDTO.getPassword())
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
         }
     }
 }
--- src/test/java/com/openclassrooms/datashare/service/UserServiceTest.java
+++ src/test/java/com/openclassrooms/datashare/service/UserServiceTest.java
@@ -5,33 +5,41 @@ import com.openclassrooms.datashare.configuration.security.CustomUserDetailServi
 import com.openclassrooms.datashare.entities.User;
 import com.openclassrooms.datashare.repository.UserRepository;
 import com.openclassrooms.datashare.validator.UserValidator;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
     private static final String LOGIN = "test@gmail.com";
     private static final String PASSWORD = "correctPassword1234*";
+    private static final String JWT = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJUZXN0VXNlciIsImlhdCI6MTc3MDkxMjQ0OCwiZXhwIjoxNzcwOTE2MDQ4fQ.y9y3IlLH5exRutwafO1tL33mEyFYcFdx0NQotj06y1I";
 
     @Mock
     private UserValidator validator;
     @Mock
     private UserRepository repository;
     @Mock
-    private AuthenticationManager authenticationManager;
+    private DaoAuthenticationProvider authenticationManager;
     @Mock
     private CustomUserDetailService userDetailService;
     @Mock
@@ -77,6 +85,7 @@ public class UserServiceTest {
             user.setLogin(LOGIN);
             user.setPassword(PASSWORD);
             when(passwordEncoder.encode(PASSWORD)).thenReturn(PASSWORD);
+            doNothing().when(validator).validate(any(User.class));
             when(repository.findByLogin(any())).thenReturn(Optional.empty());
 
             // WHEN
@@ -86,6 +95,86 @@ public class UserServiceTest {
             ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
             verify(repository).save(userCaptor.capture());
             assertThat(userCaptor.getValue()).isEqualTo(user);
+        }
+    }
+
+    @Nested
+    @Tag("LoginTests")
+    @DisplayName("Se connecter")
+    class LoginTests {
+
+        @Test
+        @DisplayName("Avec un login qui n'existe pas renvoie une erreur")
+        public void test_connect_user_with_unknow_login_throws_IllegalArgumentException(){
+            // GIVEN
+            when(repository.findByLogin(any())).thenReturn(Optional.empty());
+
+            //THEN
+            verify(passwordEncoder, times(0)).matches(any(String.class), any(String.class));
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> service.login(LOGIN, PASSWORD));
+        }
+
+        @Test
+        @DisplayName("Avec un mot de passe incorrect renvoie une erreur")
+        public void test_connect_user_with_bad_password_throws_IllegalArgumentException(){
+            // GIVEN
+            User user = new User();
+            user.setLogin(LOGIN);
+            user.setPassword(PASSWORD);
+            when(repository.findByLogin(any())).thenReturn(Optional.of(user));
+            when(passwordEncoder.matches(any(String.class), any(String.class))).thenReturn(false);
+
+            //WHEN
+            Assertions.assertThrows(IllegalArgumentException.class, () -> service.login(LOGIN, PASSWORD));
+
+            //THEN
+            verify(passwordEncoder, times(1)).matches(any(String.class), any(String.class));
+
+        }
+
+        @Test
+        @DisplayName("Avec un login et un mot de passe existant fonctionne")
+        public void test_connect_user(){
+            // GIVEN
+            User user = new User();
+            user.setLogin(LOGIN);
+            user.setPassword(PASSWORD);
+            when(repository.findByLogin(any())).thenReturn(Optional.of(user));
+            when(passwordEncoder.matches(any(String.class), any(String.class))).thenReturn(true);
+            when(authenticationManager.authenticate(any(Authentication.class))).thenReturn(new Authentication() {
+                @Override
+                public @NonNull Collection<? extends GrantedAuthority> getAuthorities() {return List.of();}
+                @Override
+                public @Nullable Object getCredentials() {return null;}
+                @Override
+                public @Nullable Object getDetails() {return null;}
+                @Override
+                public @Nullable Object getPrincipal() {return null;}
+                @Override
+                public boolean isAuthenticated() {return false;}
+                @Override
+                public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {}
+                @Override
+                public String getName() {return "";}
+            });
+            when(userDetailService.loadUserByUsername(any(String.class))).thenReturn(new UserDetails() {
+                @Override
+                public @NonNull Collection<? extends GrantedAuthority> getAuthorities() {return List.of();}
+                @Override
+                public @NonNull String getPassword() {return "";}
+                @Override
+                public @NonNull String getUsername() {return "";}
+            });
+            lenient().when(jwtUtils.generateToken(any(UserDetails.class))).thenReturn(JWT);
+
+            //WHEN
+            final String result = service.login(LOGIN, PASSWORD);
+
+            //THEN
+            verify(authenticationManager, times(1)).authenticate(any(Authentication.class));
+            verify(jwtUtils, times(1)).generateToken(any(UserDetails.class));
+            assertThat(result).isEqualTo(JWT);
         }
     }
 }
