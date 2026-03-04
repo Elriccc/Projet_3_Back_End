bacca0e29c1f7f908a3b8c6cfcbe14571c0333b6
elrixtrixx <elric.finot@outlook.fr>
2026-03-04 16:58:48 +0100

    feat: ajout des méthodes de la classe FileLinkService

--- src/main/java/com/openclassrooms/datashare/service/FileLinkService.java
+++ src/main/java/com/openclassrooms/datashare/service/FileLinkService.java
@@ -1,17 +1,48 @@
 package com.openclassrooms.datashare.service;
 
+import com.openclassrooms.datashare.entities.FileLink;
+import com.openclassrooms.datashare.entities.User;
 import com.openclassrooms.datashare.repository.FileLinkRepository;
+import com.openclassrooms.datashare.repository.UserRepository;
 import com.openclassrooms.datashare.validator.FileLinkValidator;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
+
 @Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class FileLinkService {
-    private FileLinkValidator validator;
-    private FileLinkRepository repository;
+    private final FileLinkValidator validator;
+    private final FileLinkRepository repository;
+    private final UserRepository userRepository;
+
+    public void saveFileLink(FileLink fileLink){
+
+
+    }
+
+    public List<FileLink> getAllFileLinksByAccount(){
+        return null;
+    }
+
+    public FileLink getFileLink(String fileId){
+
+        return null;
+    }
+
+    public String deleteFileLink(String fileId){
+
+        return null;
+    }
+
+    public FileLink updateFileLinkTags(String fileId, List<String> tags){
+
+        return null;
+    }
 }
