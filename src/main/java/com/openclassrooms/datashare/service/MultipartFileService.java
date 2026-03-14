93e70be4a69b294b43e35957d4b3d6cbda3e3a39
elrixtrixx <elric.finot@outlook.fr>
2026-03-14 20:22:27 +0100

    feat: Ajout du service addFile

--- src/main/java/com/openclassrooms/datashare/service/MultipartFileService.java
+++ src/main/java/com/openclassrooms/datashare/service/MultipartFileService.java
@@ -25,7 +25,31 @@ public class MultipartFileService {
     private String NO_USER_DIRECTORY;
 
     public void addFile(FileLink fileLink, MultipartFile multipartFile) throws IOException {
+        File file = this.getFile(fileLink, multipartFile);
+        File parent = file.getParentFile();
+        parent.mkdir();
+        try {
+            file.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(multipartFile.getBytes());
+            outputStream.close();
+        } catch(IOException e){
+            throw new IOException();
+        }
 
+    }
+
+    private @NonNull File getFile(FileLink fileLink, MultipartFile multipartFile) {
+        Objects.requireNonNull(multipartFile.getOriginalFilename());
+        String filePath = this.DATA_PATH;
+        if(fileLink.getUser() == null){
+            filePath = filePath.concat(NO_USER_DIRECTORY);
+        } else {
+            filePath = filePath.concat(fileLink.getUser().getId());
+        }
+        filePath = filePath.concat(fileLink.getId());
+        filePath = filePath.concat(multipartFile.getOriginalFilename().substring(multipartFile.getOriginalFilename().lastIndexOf(".")));
+        return new File(filePath);
     }
 
     public MultipartFile getFile(FileLink fileLink){
